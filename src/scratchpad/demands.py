571531695076fadec3a6bba423bf715358e21d22
Denis Maciel <denispmaciel@gmail.com>
2020-07-09 04:12:21 +0200

    van Ryzin 1994 simulation

--- src/scratchpad/demands.py
+++ src/scratchpad/demands.py
@@ -28,20 +28,24 @@
 
 # %%
 import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
 
 def price_strategy(day: int) -> float:
     return 30 if day <= 50 else 60
 
+
 def demand(p):
-    
     def _lamda(p):
-        q = 61 - p
-        
+        q = 100 - p
+
         if q < 0:
             return 0
         return q
-    
+
     return np.random.poisson(_lamda(p))
+
 
 prices = [price_strategy(day) for day in range(100)]
 demands = [demand(p) for p in prices]
@@ -49,7 +53,116 @@ demands = [demand(p) for p in prices]
 # %%
 import matplotlib.pyplot as plt
 
-plt.plot(range(100), demands)
+plt.plot(demands)
+plt.xlabel("Days")
+plt.ylabel("Realized Demand")
+
+# %% [markdown]
+# Simulation of Gallegos & van Ryzin
+#
+# \begin{equation}
+#     \lambda(p) = ae^{-\alpha p}
+# \end{equation}
+
+# %%
+from math import exp, e, factorial, log
+
+
+def mean_demand(p):
+    a = 20
+    alpha = 1
+    return a * exp(-alpha * p)
+
+
+# %%
+def optimal_expected_revenue(stock, t):
+    a = 20
+    lambda_star = 20 / e
+    s = sum((lambda_star * stock) ** i * 1 / factorial(i) for i in range(stock + 1))
+    return log(s)
+
+
+# %%
+def optimal_price(stock, t):
+    return optimal_expected_revenue(stock, t) - optimal_expected_revenue(stock - 1, t) + 1
+
+
+# %%
+
+from collections import namedtuple
+
+time_period = namedtuple("time_period", "simulation_id t demand price stock")
+
+results = []
+for s in range(100):
+    stock = 25
+    timeline = []
+    for t in range(1, 101):
+        p = optimal_price(stock, 100 - t)
+        lambda_ = mean_demand(p)
+        d = np.random.poisson(lambda_)
+
+        if stock <= d:  # Ran out of stock
+            timeline.append(time_period(s, t, d, p, 0))
+            break
+        else:
+            stock -= d
+            timeline.append(time_period(s, t, d, p, stock))
+
+    results.append(pd.DataFrame(timeline))
+
+results = pd.concat(results)
+
+# %%
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+
+def add_line(df: pd.DataFrame, ax: mpl.axes.Axes, y: str) -> mpl.axes.Axes:
+    return df.plot(
+        ax=ax,
+        kind="line",
+        x="t",
+        y=y,
+        color="black",
+        label=None,
+        legend=None,
+        alpha=1 / 10,
+    )
+
+
+for key, grp in results.groupby("simulation_id"):
+    ax1 = add_line(grp, ax1, "price")
+    ax2 = add_line(grp, ax2, "stock")
+
+ax1.set_ylabel("Price")
+ax2.set_ylabel("Stock")
+
+ax1.set_xlabel("")
+ax1.set_xlabel("")
+# ax1.get_xaxis().set_visible(False)
+ax1.set_xticklabels([])
+fig.tight_layout()
+fig.savefig("dflk.png")
+
+# %%
+# standanrdize = lambda x: (x - x.mean()) / x.std()
+
+# timeline[['stand_price']] = standanrdize(timeline[['price']])
+# timeline[['stand_stock']] = standanrdize(timeline[['stock']]).shift()
+
+# timeline[["price"]].plot()
+# plt.savefig("price_stock_dev.png"); plt.close()
+
+# timeline[["stand_price", "stand_stock"]].plot()
+# plt.savefig("standardized_price_stock_dev.png"); plt.close()
+
+# %%
+optimal_expected_revenue(10, 10)
+
+# %%
+mean_demand(1)
 
 # %% [markdown]
 # ## Constant Elasticity
@@ -59,13 +172,11 @@ plt.plot(range(100), demands)
 # \end{equation}
 
 # %%
-P = np.linspace(0, 100, 100)
-sigma = -0.8
-alpha = 9
-Q = alpha * P**sigma
- 
-plt.plot(Q, P)
-plt.xlabel("Quantity")
-plt.ylabel("Price")
+# P = np.linspace(0, 100, 100)
+# sigma = -0.8
+# alpha = 9
+# Q = alpha * P**sigma
 
-# %%
+# plt.plot(Q, P)
+# plt.xlabel("Quantity")
+# plt.ylabel("Price")
