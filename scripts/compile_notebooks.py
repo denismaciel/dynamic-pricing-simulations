f631e58761b9e58068fdf534513c1ec9055cb3e1
Denis Maciel <denis.maciel@dataiku>
2020-08-16 14:02:11 +0200

    simplify notebook compilation script

--- scripts/compile_notebooks.py
+++ scripts/compile_notebooks.py
@@ -1,4 +1,4 @@
-#! /home/denis/.pyenv/versions/dynamic-pricing/bin/python
+#! env python
 import sys
 import os
 from pathlib import Path
@@ -6,51 +6,68 @@ from pathlib import Path
 from itertools import groupby
 from operator import attrgetter
 
-home = Path()
+root = Path()
 
-def sync_notebooks():
-    targets = (home / "simulations").rglob("nb_*")
-    groups = groupby(sorted(targets), lambda x: attrgetter("stem")(x))
+# def sync_notebooks():
+#     targets = (home / "simulations").rglob("nb_*")
+#     groups = groupby(sorted(targets), lambda x: attrgetter("stem")(x))
 
-    # print("Found notebooks: {}".format(', '.join(str(name) for name, iter_ in groups)))
+#     # print("Found notebooks: {}".format(', '.join(str(name) for name, iter_ in groups)))
 
-    for stem, files in groups:
-        print(f"Processing {stem}")
+#     for stem, files in groups:
+#         print(f"Processing {stem}")
 
-        files = list(files)
+#         files = list(files)
+#         print(files)
 
-        if len(files) == 2:
-            notebook, py = files
-            os.system(f"jupytext --sync {notebook.absolute()}")
-        else:
-            print(f"Skipping {files}")
+#         if len(files) == 2:
+#             notebook, py = files
+#             # os.system(f"jupytext --sync {notebook.absolute()}")
+#         else:
+#             print(f"Skipping {files}")
 
+# def make_markdown(execute=False):
+#     targets = (home / "simulations").rglob("*.ipynb")
+#     output_dir = home.absolute() / "docs" / "notebooks"
+#     notebooks = " ".join([str(nb.absolute()) for nb in targets])
+#     execute = "--execute" if execute else ""
+#     cmd = f"""jupyter nbconvert \
+#             {execute} \
+#             --ExecutePreprocessor.timeout=180 \
+#             --output-dir {output_dir} \
+#             --to markdown {notebooks} \
+#             """
+#     print("Running script...")
+#     print()
+#     print(cmd)
+#     os.system(cmd)
 
-def make_markdown(execute=False):
-    targets = (home / "simulations").rglob("*.ipynb")
-    output_dir = home.absolute() / "docs" / "notebooks"
-    notebooks = " ".join([str(nb.absolute()) for nb in targets])
+notebooks = [
+    "simulations/demand/nb_reservation_price_demand",
+    # "simulations/dynamic_programming/nb_dynamic_programming",
+    # "simulations/online_network_revenue_management/sim",
+    # "simulations/thompson_sampling/nb_thompson_vs_greedy",
+]
+
+def make_markdown(path, execute=False):
+    notebook_path = Path(path + ".ipynb").absolute()
+    if not notebook_path.exists():
+        raise FileNotFoundError(f'Notebook {path} does not exist')
+    parent = notebook_path.absolute().parent.stem
+    output_dir = root.absolute() / "docs" / "notebooks" / parent
     execute = "--execute" if execute else ""
-    cmd = f"""jupyter nbconvert \
-            {execute} \
-            --ExecutePreprocessor.timeout=180 \
-            --output-dir {output_dir} \
-            --to markdown {notebooks} \
-            """
-    print("Running script...")
-    print()
-    print(cmd)
+    cmd = f"""
+    jupyter nbconvert \
+        {execute} \
+        --ExecutePreprocessor.timeout=180 \
+        --output-dir {output_dir} \
+        --to markdown {notebook_path} \
+    """
+
+    # print(cmd)
     os.system(cmd)
 
 
-if __name__ == "__main__":
-    if sys.argv[1] == "sync_notebooks":
-        sync_notebooks()
-    elif sys.argv[1] == "make_markdown":
-        if sys.argv[2] == "execute":
-            make_markdown(execute=True)
-        else:
-            make_markdown(execute=False)
-    else:
-        print("Unknown command")
+for n in notebooks:
+    make_markdown(n, execute=True)
 
